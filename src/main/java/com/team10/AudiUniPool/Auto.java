52eafa40ebeb7026ddfa3f13aebd00ed155c25e2
Filipe de Souza Andrade <andradefilipe@gmail.com>
2017-11-18 20:20:47 +0100

    dfs

--- src/main/java/com/team10/AudiUniPool/Auto.java
+++ src/main/java/com/team10/AudiUniPool/Auto.java
@@ -1,10 +1,14 @@
 package com.team10.AudiUniPool;
+
+import java.util.List;
+
 public class Auto {
 
 	private final long id;
 	private final String name;
 	private final String plate;
 	private final Boolean availability;
+	private List<Shift> shift;
 
 	public Auto(long id, String name, String drivingLicence, Boolean availability) {
 		this.id = id;
--- src/main/java/com/team10/AudiUniPool/Shift.java
+++ src/main/java/com/team10/AudiUniPool/Shift.java
@@ -1,45 +1,44 @@
 package com.team10.AudiUniPool;
 
-public class Schedule {
+public class Shift {
 	
 	private final long id;
-	private final Auto auto;
-	private final String startingDate;
-	private final String endingDate;
+	
+	private String startingDate;
+	private String endingDate;
 	private User user;
-
-	public Schedule(long id, User user, Auto auto, String startingDate, String endingDate) {
+	
+	public Shift(long id, String startingDate, String endingDate, User user) {
 		this.id = id;
-		this.auto = auto;
 		this.startingDate = startingDate;
 		this.endingDate = endingDate;
 		this.user = user;
 	}
-
-	public long getId() {
-		return id;
-	}
-
+	
+	
 	public String getStartingDate() {
 		return startingDate;
 	}
-
+	public void setStartingDate(String startingDate) {
+		this.startingDate = startingDate;
+	}
 	public String getEndingDate() {
 		return endingDate;
 	}
-
-	public Auto getAuto() {
-		return auto;
+	public void setEndingDate(String endingDate) {
+		this.endingDate = endingDate;
+	}
+	public long getId() {
+		return id;
 	}
 
 	public User getUser() {
 		return user;
 	}
 
+
 	public void setUser(User user) {
 		this.user = user;
 	}
 	
-
 }
-
