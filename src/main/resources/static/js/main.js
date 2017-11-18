b38939ade75a4035b4d6cb2760dc3fd524f2cb43
Jean Farias Roldão <jeanroldao@gmail.com>
2017-11-19 00:59:09 +0100

    frontend
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+(function(angular) {
+  var app = angular.module('PoolTest', ['ng', 'ngRoute', 'ngResource']);
+  
+  app.controller('indexController', function($scope, $http) {
+    var index = this;
+    
+    index.autos = [];
+    
+    index.schedules = [];
+    
+    function refreshData() {
+      $http.get('/listAuto').then(function(data) {
+        index.autos = data.data;
+      });
+      
+      $http.get('/listSchedule').then(function(data) {
+        index.schedules = data.data;
+      });
+
+    }
+    
+    index.requestAutoClick = function(auto) {
+      $http.get('/requestAuto?user=1&auto='+auto.id).then(function(data) {
+        refreshData()
+      });
+    };
+    
+    
+    refreshData();
+  });
+})(angular);
